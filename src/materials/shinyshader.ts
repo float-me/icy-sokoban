3cf4b6541b5cffd5f56a37fa5f16d7ca2b703588
leeyw101 <leeyw9764@naver.com>
2024-11-19 18:19:08 +0900

    small error

--- src/materials/shinyshader.ts
+++ src/materials/shinyshader.ts
@@ -18,7 +18,7 @@ const fragmentShader :string = `
 
           void main() {
             float stripPosition = time;
-            float intensity = cosineSquared(clamp(1.5 * abs(vPosition.x + vPosition.z - stripPosition) + 0.5*sabs(vPosition.y - 0.4), 0.0, 1.0));
+            float intensity = cosineSquared(clamp(1.5 * abs(vPosition.x + vPosition.z - stripPosition) + 0.5*abs(vPosition.y - 0.4), 0.0, 1.0));
   
             vec3 highlight = vec3(0.8) * intensity; // White strip
             gl_FragColor = vec4(color + highlight, 1.0);
