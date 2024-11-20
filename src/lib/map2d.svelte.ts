e5f81fa4b98b170fcb77a1d6ee17e97f2ad5c38b
float-me <main.float@proton.me>
2024-11-20 14:56:27 +0900

    logic change prep

--- src/lib/map2d.svelte.ts
+++ src/lib/map2d.svelte.ts
@@ -3,6 +3,13 @@ export function add_vector (a: vector, b: vector): vector {
   return [a[0] + b[0], a[1] + b[1]]
 }
 
+enum rot {
+  RIGHT = 0,
+  UP,
+  LEFT,
+  DOWN
+}
+
 export class Map2D {
   info: number[][] = $state([[]])
   width: number
@@ -46,5 +53,34 @@ export class Box {
     }
     this.moving = false
     boxes.push(this)
+  }
+}
+
+class Block {
+  position: vector
+  rotation: rot
+  moveableDir: rot[]
+  weight: number
+  durability: number
+  constructor (
+    position: vector,
+    rotation: rot,
+    moveableDir: rot[],
+    weight: number, // power < weight면 밀리지 않음. power == weight면 한칸 밀림. power > weight면 끝까지 밀림. 얼음 상자: 1, 나무 상자: 2. 일반 power: 2.
+    durability: number // power > durability면 블록이 파괴됨. 얼음 상자: 2.
+  ) {
+    this.position = position
+    this.rotation = rotation
+    this.moveableDir = moveableDir
+    this.weight = weight
+    this.durability = durability
+  }
+  can_move (direction: rot) {
+    for (const dir of this.moveableDir) {
+      if (direction === (this.rotation + dir) % 4) {
+        return true
+      }
+    }
+    return false
   }
 }
